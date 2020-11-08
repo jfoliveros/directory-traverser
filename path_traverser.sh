3e5ed69616c81f1227948305f3db17f2c22c8082
Julian Oliveros <oliveros@cac.co.jp>
2020-11-08 16:47:38 +0900

    Added basic implementation for linked list bash filesystem traversing

---
+++ path_traverser.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export BASE=$(pwd)
+. utilities
+cd $BASE
+
+create_linked_list
+rm -rf FoundObjects
+mkdir FoundObjects
+
+while [ $iterator -gt 0 ]
+do
+  current_node_values=$(get_node_values)
+  if [ "$current_node_values" = "" ]
+  then
+    backtrack
+  else
+    process_node $current_node_values
+    next_node=$(get_next_node)
+    remove_directory_from_path $next_node
+    forward $next_node
+  fi
+done
